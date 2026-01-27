e5d482abd65a0dfe18e4a2208e5eec4226702970
Ephrem_art_ <ephremtesema92@gmail.com>
2026-01-27 10:27:02 +0300

    second commit

--- frontend/teacher/vite.config.js
+++ frontend/teacher/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Set base to repo name for GitHub Pages
+  base: '/Gojo-Teacher-Web/',
 })
